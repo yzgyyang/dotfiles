7e46cf06835dd7d357b89cc101f9091ab4d18316
Guangyuan Yang <yzgyyang@outlook.com>
2018-03-22 03:26:59 -0400

    Upgrade the whole system: use symlink

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-cp .vimrc ~
-cp .zshrc ~
-cp .tmux.conf ~
-cp .gitconfig ~
+ln -nsf ${PWD}/vimrc ${HOME}/.vimrc
+ln -nsf ${PWD}/zshrc ${HOME}/.zshrc
+ln -nsf ${PWD}/tmux.conf ${HOME}/.tmux.conf
+ln -nsf ${PWD}/gitconfig ${HOME}/.gitconfig
