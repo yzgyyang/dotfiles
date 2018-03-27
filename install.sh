a49efd45c3dd4afb10bc7dbb0e0dcb32151c2851
Guangyuan Yang <yzgyyang@outlook.com>
2018-03-27 18:57:37 -0400

    Add ssh_config: ssh configuration file

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -nsf ${PWD}/vimrc ${HOME}/.vimrc
 ln -nsf ${PWD}/zshrc ${HOME}/.zshrc
 ln -nsf ${PWD}/tmux.conf ${HOME}/.tmux.conf
 ln -nsf ${PWD}/gitconfig ${HOME}/.gitconfig
+mkdir -p ${HOME}/.ssh
+ln -nsf ${PWD}/ssh_config ${HOME}/.ssh/config
