31884d2722bbf0436b3e26786740a9f30e289fe0
Guangyuan Yang <yzgyyang@outlook.com>
2018-03-30 03:19:24 -0400

    Add .gitignore_global: global gitignore file

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -nsf ${PWD}/vimrc ${HOME}/.vimrc
 ln -nsf ${PWD}/zshrc ${HOME}/.zshrc
 ln -nsf ${PWD}/tmux.conf ${HOME}/.tmux.conf
 ln -nsf ${PWD}/gitconfig ${HOME}/.gitconfig
+ln -nsf ${PWD}/gitignore_global ${HOME}/.gitignore_global
 mkdir -p ${HOME}/.ssh
 ln -nsf ${PWD}/ssh_config ${HOME}/.ssh/config
