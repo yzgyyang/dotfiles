2dbf093288ed8eaffbba42e7a1eaf6ea07553994
Guangyuan Yang <yzgyyang@outlook.com>
2019-09-09 14:25:24 -0400

    Update scripts/zsh_dracula.sh: also install lib

--- scripts/zsh_dracula.sh
+++ scripts/zsh_dracula.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 
 rm -rf /tmp/dracula-zsh
+rm -rf ~/.oh-my-zsh/themes/dracula.zsh-theme
 git clone https://github.com/dracula/zsh.git /tmp/dracula-zsh
-mv /tmp/dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+cp /tmp/dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+rsync -a /tmp/dracula-zsh/lib/ ~/.oh-my-zsh/themes/lib
 rm -rf /tmp/dracula-zsh
